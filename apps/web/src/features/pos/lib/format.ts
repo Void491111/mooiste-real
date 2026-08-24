a56e609952ddf3346472be747199183ed00a9f85
unknown <voidw87@gmail.com>
2026-08-24 23:59:38 +0700

    membuat isi file format.ts

---
+++ apps/web/src/features/pos/lib/format.ts
@@ -0,0 +1,12 @@
+import { POS_CONFIG } from "@/config/pos.config";
+
+const { locale, prefix, fractionDigits } = POS_CONFIG.currency;
+
+const formatter = new Intl.NumberFormat(locale, {
+    minimumFractionDigits: fractionDigits,
+    maximumFractionDigits: fractionDigits,
+});
+
+export function formatMoney(value: number) {
+    return `{prefix} ${formatter.format(value)}` 
+}
