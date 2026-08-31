18428082220b63c64c625c40680b65c89a1848e7
unknown <voidw87@gmail.com>
2026-08-31 17:59:02 +0700

    update app.module.ts

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,12 +3,13 @@ import { APP_GUARD } from "@nestjs/core";
 import { AuthModule } from "./auth/auth.module";
 import { JwtAuthGuard } from "./auth/jwt.guard";
 import { RolesGuard } from "./auth/roles.guard";
+import { DashboardModule } from "./dashboard/dashboard.module";
 import { MenuModule } from "./menu/menu.module";
 import { OrderModule } from "./order/order.module";
 import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [PrismaModule, AuthModule, MenuModule, OrderModule],
+  imports: [PrismaModule, AuthModule, MenuModule, OrderModule, DashboardModule],
   providers: [
     { provide: APP_GUARD, useClass: JwtAuthGuard },
     { provide: APP_GUARD, useClass: RolesGuard },
